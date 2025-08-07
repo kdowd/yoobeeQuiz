1ab562b9cdb8ee93518e2e612c4f8633e89cfb34
kdowd <kevin.dowd@gmail.com>
2025-08-07 21:20:17 +1200

    change background colour

--- main.js
+++ main.js
@@ -68,6 +68,6 @@ const beforeUnloadHandler = (event) => {
   event.returnValue = true;
 };
 
-if (location.hostname != "127.0.0.1") {
+if (location.hostname != "127.0.0.63") {
   window.addEventListener("beforeunload", beforeUnloadHandler);
 }
